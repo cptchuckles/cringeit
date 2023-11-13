8fd0b16a5c3bc3fe0a3895d04e65e5f6f2b0bc56
Grendel <jcg@grendel.world>
2023-11-13 12:09:54 -0600

    routes: use table name instead of model name

--- flask_app/controllers/controller_base.py
+++ flask_app/controllers/controller_base.py
@@ -7,34 +7,32 @@ class ControllerBase:
     def __init__(self, model: ModelBase):
         self.model = model
         self.model_name = self.model.__name__.lower()
-        self.item_name = self.model_name
-        self.collection_name = self.model_name + "s"
 
     def register_crud_routes(self):
-        @app.route(f"/{self.model_name}", endpoint=f"{self.model_name}/index")
+        @app.route(f"/{self.model.table}", endpoint=f"{self.model.table}/index")
         def index(): return self.index()
 
-        @app.route(f"/{self.model_name}/<int:id>", endpoint=f"{self.model_name}/show")
+        @app.route(f"/{self.model.table}/<int:id>", endpoint=f"{self.model.table}/show")
         def show(id: int): return self.show(id)
 
-        @app.route(f"/{self.model_name}/new", endpoint=f"{self.model_name}_new")
+        @app.route(f"/{self.model.table}/new", endpoint=f"{self.model.table}/new")
         def new(): return self.new()
 
-        @app.route(f"/{self.model_name}/create", methods=["POST"], endpoint=f"{self.model_name}/create")
+        @app.route(f"/{self.model.table}/create", methods=["POST"], endpoint=f"{self.model.table}/create")
         def create(): return self.create(request.form)
 
-        @app.route(f"/{self.model_name}/<int:id>/edit", endpoint=f"{self.model_name}/edit")
+        @app.route(f"/{self.model.table}/<int:id>/edit", endpoint=f"{self.model.table}/edit")
         def edit(id: int): return self.edit(id)
 
-        @app.route(f"/{self.model_name}/update", methods=["POST"], endpoint=f"{self.model_name}/update")
+        @app.route(f"/{self.model.table}/update", methods=["POST"], endpoint=f"{self.model.table}/update")
         def update(): return self.update(request.form)
 
-        @app.route(f"/{self.model_name}/<int:id>/delete", endpoint=f"{self.model_name}/delete")
+        @app.route(f"/{self.model.table}/<int:id>/delete", endpoint=f"{self.model.table}/delete")
         def delete(id: int): return self.delete(id)
 
     def index(self):
         collection = self.model.get_all()
-        return render_template(f"views/{self.model_name}/index.html", **{self.collection_name: collection})
+        return render_template(f"views/{self.model_name}/index.html", **{self.model.table: collection})
 
     def show(self, id: int):
         item = self.model.get_by_id(id)
@@ -48,7 +46,7 @@ class ControllerBase:
 
     def create(self, form_data):
         new_id = self.model.create(form_data)
-        return redirect(f"/{self.model_name}/{new_id}")
+        return redirect(f"/{self.model.table}/{new_id}")
 
     def edit(self, id: int):
         item = self.model.get_by_id(id)
@@ -58,12 +56,9 @@ class ControllerBase:
             return render_template(f"/views/{self.model_name}/edit.html", **{self.item_name: item})
 
     def update(self, form_data):
-        item = self.model.update(form_data)
-        if item is None:
-            return abort(500)
-        else:
-            return redirect(f"/{self.model_name}/{item.id}")
+        self.model.update(form_data)
+        return redirect(f"/{self.model.table}/{form_data.get('id')}")
 
     def delete(self, id: int):
         self.model.delete(id)
-        return redirect(f"/{self.model_name}")
+        return redirect(f"/{self.model.table}")
