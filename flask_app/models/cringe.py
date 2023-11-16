a315893d6fab992497915ace02f300bf2f3dbd83
Grendel <jcg@grendel.world>
2023-11-16 13:47:25 -0600

    models: add Cringe

---
+++ flask_app/models/cringe.py
@@ -0,0 +1,39 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app.models.model_base import ModelBase
+from flask_app.models import user
+
+
+class Cringe(ModelBase):
+    table = "cringe"
+    fields = [
+        "id",
+        "user_id",
+        "headline",
+        "url",
+        "description",
+        "created_at",
+        "updated_at",
+    ]
+    pk = "id"
+
+    @classmethod
+    def get_by_id(cls, id: int):
+        return super().join_one(id, user.User)
+
+    @classmethod
+    def get_all(cls):
+        model = cls.__name__.lower()
+        query = f"""
+            SELECT * FROM {cls.table}
+            JOIN {user.User.table}
+            ON {user.User.table}.{model}_id = {cls.table}.id
+        """
+        view = connectToMySQL(cls.db).query_db(query)
+
+        items = []
+        for row in view:
+            item = cls(row)
+            setattr(item, "user", user.User(row))
+            items.append(item)
+
+        return items
