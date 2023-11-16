e5aa235a2d6232c0b47de59a566e7f771c23cb41
Grendel <jcg@grendel.world>
2023-11-16 15:27:18 -0600

    models: add CringeRating

--- flask_app/models/cringe.py
+++ flask_app/models/cringe.py
@@ -1,6 +1,6 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask_app.models.model_base import ModelBase
-from flask_app.models import user
+from flask_app.models import user, cringe_rating
 
 
 class Cringe(ModelBase):
@@ -14,15 +14,44 @@ class Cringe(ModelBase):
 
     @classmethod
     def get_by_id(cls, id: int):
-        return super().join_one(id, user.User)
+        users = user.User.table
+        ratings = cringe_rating.CringeRating.table
+
+        query = f"""
+            SELECT
+                *,
+                SUM(COALESCE({ratings}.delta, 0)) AS rating
+            FROM {cls.table}
+            JOIN {users}
+                ON {cls.table}.user_id = {users}.id
+            LEFT JOIN {ratings}
+                ON {ratings}.cringe_id = {cls.table}.id
+            WHERE {cls.table}.id = %(id)s
+        """
+        view = connectToMySQL(cls.db).query_db(query, {"id": id})
+        if not view:
+            return None
+
+        item = cls(view[0])
+        setattr(item, "rating", int(view[0].get("rating")))
+
+        return item
 
     @classmethod
     def get_all(cls):
         users = user.User.table
+        ratings = cringe_rating.CringeRating.table
+
         query = f"""
-            SELECT * FROM {cls.table}
+            SELECT
+                *,
+                SUM(COALESCE({ratings}.delta, 0)) AS rating
+            FROM {cls.table}
             JOIN {users}
                 ON {cls.table}.user_id = {users}.id
+            LEFT JOIN {ratings}
+                ON {ratings}.cringe_id = {cls.table}.id
+            GROUP BY {cls.table}.id
         """
         view = connectToMySQL(cls.db).query_db(query)
 
@@ -30,6 +59,7 @@ class Cringe(ModelBase):
         for row in view:
             item = cls(row)
             setattr(item, "user", user.User(row))
+            setattr(item, "rating", row.get("rating"))
             items.append(item)
 
         return items
---
+++ flask_app/models/cringe_rating.py
@@ -0,0 +1,37 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app.models.model_base import ModelBase
+
+
+class CringeRating(ModelBase):
+    table = "cringe_ratings"
+    fields = [
+        "cringe_id",
+        "user_id",
+        "delta",
+    ]
+
+    @classmethod
+    def update(cls, data: dict):
+        query = f"""
+            UPDATE {cls.table}
+            SET delta = %(delta)s
+            WHERE id = %(id)s
+        """
+        return connectToMySQL(cls.db).query_db(query, data)
+
+    @classmethod
+    def delete_rating_for_cringe_by_user(cls, cringe_id: int, user_id: int):
+        query = f"""
+            DELETE FROM {cls.table}
+            WHERE user_id = %(user_id)s AND cringe_id = %(cringe_id)s;
+        """
+        return connectToMySQL(cls.db).query_db(query, {"user_id": user_id, "cringe_id": cringe_id})
+
+    @classmethod
+    def get_rating_for_cringe_by_user(cls, cringe_id: int, user_id: int):
+        query = f"""
+            SELECT * FROM {cls.table}
+            WHERE user_id = %(user_id)s AND cringe_id = %(cringe_id)s;
+        """
+        view = connectToMySQL(cls.db).query_db(query, {"user_id": user_id, "cringe_id": cringe_id})
+        return cls(view[0]) if view else None
