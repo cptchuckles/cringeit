9886cbae82ad3442172a51977ae297f1d8ea768d
Grendel <jcg@grendel.world>
2023-11-17 12:16:32 -0600

    add as_self rule. also bugfix getting id from dict

--- flask_app/config/policy.py
+++ flask_app/config/policy.py
@@ -2,7 +2,7 @@ from flask import redirect, session, flash
 from flask_app.models.user import User
 
 
-def authorize_view(as_admin=False, as_owner=False, anonymous_to="/", unauthorized_to="/dashboard"):
+def authorize_view(as_admin=False, as_owner=False, as_self=False, anonymous_to="/", unauthorized_to="/dashboard"):
     def decorator(endpoint):
         def policy(*args, **kwargs):
             if "user_id" not in session:
@@ -10,24 +10,37 @@ def authorize_view(as_admin=False, as_owner=False, anonymous_to="/", unauthorize
                 return redirect(anonymous_to)
 
             user = User.get_by_id(session["user_id"])
+            authorized = True
 
-            if as_admin is True:
-                if not user.is_admin:
-                    flash("You are not authorized to activate this", "error")
-                    return redirect(unauthorized_to)
-            elif as_owner is True and not user.is_admin:
-                controller = args[0]
-                if type(args[1]) is dict:
-                    id = args[1].get("id")
-                else:
-                    id = args[1]
-                item = controller.model.get_by_id(id)
-                owner_id = getattr(item, "user_id", None)
-                if owner_id is not None and user.id != owner_id:
-                    flash("You are not authorized to activate this", "error")
-                    return redirect(unauthorized_to)
+            if not user.is_admin:
+                if as_admin is True:
+                    authorized = False
 
-                kwargs[controller.model.__name__.lower()] = item
+                elif as_owner is True:
+                    controller = args[0]
+                    if isinstance(args[1], dict):
+                        id = int(args[1].get("id"))
+                    else:
+                        id = args[1]
+                    item = controller.model.get_by_id(id)
+                    owner_id = getattr(item, "user_id", None)
+                    if owner_id is not None and user.id != owner_id:
+                        authorized = False
+                    else:
+                        kwargs[controller.model.__name__.lower()] = item
+
+                elif as_self is True:
+                    if isinstance(args[1], dict):
+                        id = int(args[1].get("id"))
+                    else:
+                        id = args[1]
+                    if user.id != id:
+                        print(f"Current user id: {user.id} - Self id: {id} - Mismatch!")
+                        authorized = False
+
+            if authorized is False:
+                flash("You are not authorized to activate this", "error")
+                return redirect(unauthorized_to)
 
             kwargs["user"] = user
             return endpoint(*args, **kwargs)
