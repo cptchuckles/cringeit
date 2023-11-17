f3de94c99b1ab6c20a4eccff028964b516c1eb2d
Grendel <jcg@grendel.world>
2023-11-16 22:59:50 -0600

    add authorize_view policy decorator

---
+++ flask_app/config/policy.py
@@ -0,0 +1,27 @@
+from flask import redirect, session
+from flask_app.models.user import User
+
+
+def authorize_view(as_admin=False, as_id=None, anonymous_to="/", unauthorized_to="/dashboard"):
+    def decorator(render):
+        def policy(controller):
+            if "user_id" not in session:
+                print("not authenticated!")
+                return redirect(anonymous_to)
+
+            if as_id is not None and session["user_id"] != as_id:
+                print(f"user id invalid: expected {as_id}, got {session['user_id']}")
+                return redirect(unauthorized_to)
+
+            user = User.get_by_id(session["user_id"])
+
+            if as_admin is True:
+                if not user.is_admin:
+                    print("user is not admin!")
+                    return redirect(unauthorized_to)
+
+            return render(controller, user=user)
+
+        return policy
+
+    return decorator
