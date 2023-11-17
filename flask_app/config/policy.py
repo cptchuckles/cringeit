bf4d1ecb189a20f913a0267c0c81622766659903
Grendel <jcg@grendel.world>
2023-11-17 11:32:03 -0600

    remove debug prints

--- flask_app/config/policy.py
+++ flask_app/config/policy.py
@@ -6,18 +6,15 @@ def authorize_view(as_admin=False, as_id=None, anonymous_to="/", unauthorized_to
     def decorator(render):
         def policy(*args, **kwargs):
             if "user_id" not in session:
-                print("not authenticated!")
                 return redirect(anonymous_to)
 
             if as_id is not None and session["user_id"] != as_id:
-                print(f"user id invalid: expected {as_id}, got {session['user_id']}")
                 return redirect(unauthorized_to)
 
             user = User.get_by_id(session["user_id"])
 
             if as_admin is True:
                 if not user.is_admin:
-                    print("user is not admin!")
                     return redirect(unauthorized_to)
 
             kwargs["user"] = user
