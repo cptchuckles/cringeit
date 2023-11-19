aa557ebd389b8895070f3c34984b4dcaa30c3e40
Grendel <jcg@grendel.world>
2023-11-19 13:52:48 -0600

    Show me the query that went wrong!

--- flask_app/config/mysqlconnection.py
+++ flask_app/config/mysqlconnection.py
@@ -39,7 +39,7 @@ class MySQLConnection:
                     self.connection.commit()
             except Exception as e:
                 # if the query fails the method will return FALSE
-                print("Something went wrong", e)
+                print("Something went wrong\n", query, "\n", e)
                 return False
             finally:
                 # close the connection
