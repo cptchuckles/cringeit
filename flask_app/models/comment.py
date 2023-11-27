d6016259bb8babb5cac4fac885ae75eba052c7ad
Grendel <jcg@grendel.world>
2023-11-26 18:25:04 -0600

    add get_by_id override that gets everything needed to show 1 comment

--- flask_app/models/comment.py
+++ flask_app/models/comment.py
@@ -48,6 +48,40 @@ class Comment(ModelBase):
         return [item for item in items.values() if item.parent_comment_id is None]
 
     @classmethod
+    def get_by_id(cls, id: int):
+        users = user.User.table
+        ratings = comment_rating.CommentRating.table
+        query = f"""
+            SELECT
+                {cls.table}.*,
+                {users}.username AS username,
+                parent_{users}.username AS parent_comment_username,
+                SUM(COALESCE({ratings}.delta, 0)) AS rating
+            FROM {cls.table}
+            JOIN {users}
+                ON {cls.table}.user_id = {users}.id
+            LEFT JOIN (
+                SELECT {cls.table}.id, {users}.username FROM {cls.table}
+                JOIN {users} ON {users}.id = {cls.table}.user_id
+            ) AS parent_{users}
+                ON parent_{users}.id = {cls.table}.parent_comment_id
+            LEFT JOIN {ratings}
+                ON {ratings}.comment_id = {cls.table}.id
+            WHERE {cls.table}.id = %(id)s
+        """
+        view = connectToMySQL(cls.db).query_db(query, {"id": id})
+        if not view:
+            return None
+
+        item = cls(view[0])
+        setattr(item, "username", view[0].get("username"))
+        setattr(item, "rating", view[0].get("rating"))
+        if (parent_username := view[0].get("parent_comment_username")) is not None:
+            setattr(item, "parent_comment_username", parent_username)
+
+        return item
+
+    @classmethod
     def update(cls, form_data):
         query = f"""
             UPDATE {cls.table}
