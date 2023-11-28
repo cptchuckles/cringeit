135cc4f0f3ccc44e83568468cc2d0b6887df029c
Grendel <jcg@grendel.world>
2023-11-27 22:23:27 -0600

    use API to update comments and reflect changes via AJAX

--- flask_app/static/js/types/comments.js
+++ flask_app/static/js/types/comments.js
@@ -1,3 +1,10 @@
+/**
+ * Returns an HTMLElement of the specified elementType,
+ * with the specified attributes.
+ *
+ * @param {string} elementType
+ * @returns {HTMLElement}
+ */
 function MakeElement(elementType, attributes) {
     const element = Object.assign(document.createElement(elementType), attributes);
     for (const styleClass of attributes.classes || []) {
@@ -10,77 +17,40 @@ function MakeElement(elementType, attributes) {
 }
 
 class CommentForm extends HTMLElement {
-    constructor(params = {}) {
+    constructor({
+        cringeId,
+        commentId,
+        username,
+        content,
+        parentCommentId,
+        parentCommentUsername,
+        hiddenElement,
+        focusOnLoad,
+    } = {}) {
         super();
 
-        this.commentId = params.commentId;
+        this.commentId = commentId;
+        this.username = username;
 
-        this.isEditForm = "commentId" in params;
-        this.parentCommentId = params.parentCommentId;
-        this.parentUsername = params.parentUsername;
+        this.parentCommentId = parentCommentId;
+        this.parentCommentUsername = parentCommentUsername;
 
-        this.hiddenElement = params.hiddenElement;
-        this.cringeId = params.cringeId;
-        this.content = params.content;
-        this.focusOnLoad = params.focusOnLoad;
-
-        this.data = null;
-    }
-
-    keyboardControlsHandler(event) {
-        if (event.ctrlKey && event.key === "Enter") {
-            event.preventDefault();
-            event.target.form.dispatchEvent(new Event("submit"));
-        }
-        else if (this.hiddenElement && event.key === "Escape") {
-            this.destroy();
-        }
-    }
-
-    destroy() {
-        this.parentElement.appendChild(this.hiddenElement);
-        this.remove();
-    }
-
-    async post(ev) {
-        ev.preventDefault();
-        const form = ev.target;
-        const formData = new FormData(form);
-        const response = await fetch("/api/comments/create", { method: "POST", body: formData });
-        const [status, json] = [response.status, await response.json()];
-        if (status === 201) {
-            const comment = await fetch(`/api/comments/${json.id}`).then(resp => resp.json())
-            if (this.parentCommentId) {
-                const parent = document.getElementById(`comment-${this.parentCommentId}`).parentElement;
-                const reply = parent.addReply(comment);
-                reply.focus();
-                this.destroy();
-            }
-            else {
-                const commentSection = document.getElementById("comments");
-                const newComment = new CringeComment(comment);
-                commentSection.insertBefore(newComment, commentSection.firstChild);
-                newComment.focus();
-                form.reset();
-            }
-            return true;
-        }
-        else {
-            const errorMessage = "Could not post comment:\n" + json.errors.join("\n");
-            alert(errorMessage);
-            console.log(errorMessage);
-            return false;
-        }
+        this.hiddenElement = hiddenElement;
+        this.cringeId = cringeId;
+        this.content = content;
+        this.focusOnLoad = focusOnLoad;
+        this.isEditForm = !!this.commentId;
     }
 
     connectedCallback() {
         this.cringeId = this.cringeId || this.getAttribute("cringe-id");
 
-        const form = MakeElement("form", {
+        /** @type {HTMLFormElement} */
+        const form =  MakeElement("form", {
             method: "POST",
             classes: ["wide", "column"]
         });
-        form.addEventListener("submit", ev => this.post(ev))
+        form.addEventListener("submit", ev => this.isEditForm ? this.update(ev) : this.post(ev))
 
         if (this.commentId) {
             form.appendChild(
@@ -115,11 +85,11 @@ class CommentForm extends HTMLElement {
         const textArea = MakeElement("textarea", {
             name: "content",
             rows: 4,
-            placeholder: this.parentUsername
-                ? `Reply to ${this.parentUsername}`
+            placeholder: this.parentCommentUsername
+                ? `Reply to ${this.parentCommentUsername}`
                 : "Write a Comment"
         });
-        textArea.addEventListener("keydown", ev => this.keyboardControlsHandler(ev));
+        textArea.addEventListener("keydown", ev => this.keydownEventHandler(ev));
         if (this.content) {
             textArea.value = this.content;
             textArea.rows = Math.max(4, this.content.split("\n").length);
@@ -158,64 +128,130 @@ class CommentForm extends HTMLElement {
             textArea.selectionStart = textArea.value.length;
         }
     }
+
+    /**
+     * KeyboardEvent handler for submitting and cancelling the form.
+     *
+     * @param {KeyboardEvent} event
+     */
+    keydownEventHandler(event) {
+        if (event.ctrlKey && event.key === "Enter") {
+            event.preventDefault();
+            event.target.form.dispatchEvent(new Event("submit"));
+        }
+        else if (this.hiddenElement && event.key === "Escape") {
+            this.destroy();
+        }
+    }
+
+    /**
+     * Delete the form and restore the element that it had hidden.
+     */
+    destroy() {
+        if (!this.hiddenElement) {
+            return;
+        }
+        this.parentElement.appendChild(this.hiddenElement);
+        this.remove();
+    }
+
+    /**
+     * POST a new comment to /api/comments/create
+     *
+     * @param {SubmitEvent} ev
+     */
+    async post(ev) {
+        ev.preventDefault();
+        const form = /** @type {HTMLFormElement} */ (ev.target);
+        const formData = new FormData(form);
+        const response = await fetch("/api/comments/create", { method: "POST", body: formData });
+        const json = await response.json();
+        if (response.status === 201) {
+            const newData = json.data;
+            newData.rating = 0;
+            newData.username = authUser.username;
+            if (this.parentCommentId) {
+                /** @type {CringeComment} */
+                const parent = document.getElementById(`comment-${this.parentCommentId}`);
+                newData.parentCommentId = parent.commentId;
+                newData.parentCommentUsername = parent.username;
+                const reply = new CringeComment(newData);
+                parent.addReply(reply);
+                reply.focus();
+                this.destroy();
+            }
+            else {
+                const commentTree = /** @type {CringeCommentTree} */ (document.commentTree);
+                const newComment = new CringeComment(newData);
+                commentTree.insertBefore(newComment, commentTree.firstChild);
+                newComment.focus();
+                form.reset();
+            }
+            return true;
+        }
+        else {
+            const errorMessage = ["Could not post comment:", ...json.errors].join("\n\t- ");
+            console.error(errorMessage);
+            return false;
+        }
+    }
+
+    /**
+     * POST the updated comment to /api/comments/{commentId}/update 
+     *
+     * @param {SubmitEvent} ev
+     */
+    async update(ev) {
+        ev.preventDefault();
+        const form = /** @type {HTMLFormElement} */ (ev.target);
+        const formData = new FormData(form);
+        const response = await fetch("/api/comments/update", { method: "POST", body: formData });
+        const json = await response.json();
+        if (response.status === 200) {
+            const comment = this.closest("cringe-comment");
+            comment.content = json.data.content;
+            this.hiddenElement.querySelector(".content").textContent = json.data.content;
+            this.destroy();
+        }
+        else {
+            const errorMessage = ["Could not update comment for some reason:", json.errors].join("\n\t- ");
+            console.error(errorMessage);
+            form.getElementsByTagName("textarea")[0].style.backgroundColor = "yellow";
+        }
+    }
 }
 
 class CringeComment extends HTMLElement {
-    static edit(cringeId, commentId) {
-        const commentBody = document.querySelector(`#comment-${commentId} .comment-body`);
-        const content = commentBody.querySelector(".content").textContent;
-        const editForm = new CommentForm({
-            hiddenElement: commentBody,
-            cringeId: cringeId,
-            commentId: commentId,
-            content: content,
-            focusOnLoad: true,
-        });
-        commentBody.parentElement.appendChild(editForm);
-        commentBody.remove();
-    }
+    constructor({
+        id,
+        cringe_id,
+        user_id,
+        username,
+        content,
+        rating,
+        replies,
+        parent_comment_id,
+        parent_comment_username,
+        created_at,
 
-    static showReplyForm(link, cringeId, parentCommentId, parentUsername) {
-        const linkSpan = link;
-        const replyForm = new CommentForm({
-            hiddenElement: linkSpan,
-            cringeId: cringeId,
-            parentCommentId: parentCommentId,
-            parentUsername: parentUsername,
-            focusOnLoad: true,
-        });
-        linkSpan.parentElement.appendChild(replyForm);
-        linkSpan.remove();
-    }
-
-    static async populateCringeComments() {
-        const commentsContainer = document.getElementById("comments");
-
-        const response = await fetch("/api" + document.location.pathname + "/comments");
-        const comments = await response.json();
-
-        for (let i=comments.length; i-- > 0;) {
-            commentsContainer.appendChild(new CringeComment(comments[i]));
-        }
-
-        const hash = document.location.hash;
-        document.location.hash = '';
-        document.location.hash = hash;
-    }
-
-    constructor(data) {
+        cringeId,
+        userId,
+        parentCommentId,
+        parentCommentUsername,
+        createdAt,
+    } = {}) {
         super();
 
-        this.commentId = data.id;
-        this.cringeId = data.cringeId || data.cringe_id;
-        this.userId = data.userId || data.user_id;
-        this.username = data.username;
-        this.content = data.content;
-        this.rating = data.rating;
-        this.parentCommentId = data.parentCommentId || data.parent_comment_id;
-        this.parentCommentUsername = data.parentCommentUsername || data.parent_comment_username;
-
-        this.replies = data.replies || [];
+        this.commentId = id;
+        this.cringeId = cringeId || cringe_id;
+        this.userId = userId || user_id;
+        this.username = username;
+        this.content = content;
+        this.rating = rating;
+        this.parentCommentId = parentCommentId || parent_comment_id;
+        this.parentCommentUsername = parentCommentUsername || parent_comment_username;
+        this.replies = replies || [];
+        this.createdAt = created_at || createdAt;
     }
 
     connectedCallback() {
@@ -223,13 +259,16 @@ class CringeComment extends HTMLElement {
         this.parentCommentId = this.getAttribute("parent-comment-id") || this.parentCommentId;
         this.userId = this.getAttribute("user-id") || this.userId;
         this.username = this.getAttribute("username") || this.username;
+        this.rating = this.getAttribute("rating") || this.rating;
 
-        this.commentId = Number(this.commentId);
         this.parentCommentId = Number(this.parentCommentId);
         this.userId = Number(this.userId);
 
+        this.id = `comment-${this.commentId}`;
+        this.content = this.content || this.textContent;
+        this.textContent = "";
+
         const root = MakeElement("div", {
-            id: `comment-${this.commentId}`,
             classes: ["comment", "row", "card"],
         });
 
@@ -268,27 +307,24 @@ class CringeComment extends HTMLElement {
         body.appendChild(userHeading);
 
         const commentBody = MakeElement("div", { classes: ["comment-body"] });
-
         commentBody.appendChild(MakeElement("p", {
             classes: ["short", "content", "pre-space"],
             textContent: this.content,
         }));
 
         const linkSpan = MakeElement("span", {
-            classes: ["short", "row"],
+            classes: ["comment-links", "short", "row"],
             styleRules: { fontSize: ".8em" },
         });
         const replyLink = document.createElement("a");
         replyLink.textContent = "Reply";
-        replyLink.addEventListener("click", () => {
-            CringeComment.showReplyForm(linkSpan, this.cringeId, this.commentId, this.username);
-        });
+        replyLink.addEventListener("click", () => this.showReplyForm());
         linkSpan.appendChild(replyLink);
 
         if (authUser.id === this.userId) {
             const editLink = document.createElement("a");
             editLink.textContent = "Edit";
-            editLink.addEventListener("click", () => CringeComment.edit(this.cringeId, this.commentId));
+            editLink.addEventListener("click", () => this.showEditForm());
             linkSpan.appendChild(editLink);
 
             linkSpan.appendChild(MakeElement("a", {
@@ -311,22 +347,87 @@ class CringeComment extends HTMLElement {
         }
     }
 
+    /**
+     * Render a CommentForm to edit this CringeComment's content
+     */
+    showEditForm() {
+        const commentBody = this.querySelector(`.comment-body`);
+        const editForm = new CommentForm({
+            hiddenElement: commentBody,
+            commentId: this.commentId,
+            cringeId: this.cringeId,
+            content: this.content,
+            focusOnLoad: true,
+        });
+        commentBody.parentElement.appendChild(editForm);
+        commentBody.remove();
+    }
+
+    /**
+     * Render a CommentForm to reply to this CringeComment
+     */
+    showReplyForm() {
+        const linkSpan = this.getElementsByClassName("comment-links")[0];
+        const replyForm = new CommentForm({
+            cringeId: this.cringeId,
+            parentCommentId: this.commentId,
+            parentCommentUsername: this.username,
+            hiddenElement: linkSpan,
+            focusOnLoad: true,
+        });
+        linkSpan.parentElement.appendChild(replyForm);
+        linkSpan.remove();
+    }
+
+    /**
+     * Adds a reply under this CringeComment.
+     *
+     * @param {CringeComment} reply The CringeComment to append to this.replies
+     */
     addReply(reply) {
         let replies = this.querySelector(".comment-replies");
         if (replies === null) {
             replies = MakeElement("div", { classes: ["comment-replies"] });
             this.appendChild(replies);
         }
-        const replyInstance = new CringeComment(reply);
-        replies.appendChild(replyInstance);
-        return replyInstance;
+        replies.appendChild(reply);
     }
 
+    /**
+     * Target the document's location hash on this CringeComment's id.
+     */
     focus() {
         document.location.hash = '';
         document.location.hash = `#comment-${this.commentId}`;
     }
 }
 
+class CringeCommentTree extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    async connectedCallback() {
+        this.classList.add("column");
+        this.style.gap = "0";
+
+        document.commentTree = this;
+
+        const response = await fetch("/api" + document.location.pathname + "/comments");
+        const comments = await response.json();
+
+        // CringeComments arrive in forward chronological order.
+        // Display them from newest to oldest.
+        for (let i=comments.length; i-- > 0;) {
+            this.appendChild(new CringeComment(comments[i]));
+        }
+
+        const hash = document.location.hash;
+        document.location.hash = '';
+        document.location.hash = hash;
+    }
+}
+
 customElements.define("comment-form", CommentForm);
 customElements.define("cringe-comment", CringeComment);
+customElements.define("cringe-comment-tree", CringeCommentTree);
